266772c030073b00e5729fd7e6b65b1baa4644eb
mehedi <bd34017053@gmail.com>
2023-05-23 12:26:44 +0600

    time remender

---
+++ CountDown.js
@@ -0,0 +1,44 @@
+const day=document.getElementById("b1");
+const Hour=document.getElementById("b2");
+const Min=document.getElementById("b3");
+const Second=document.getElementById("b4");
+
+
+
+function CountDown()
+{
+    const eventTime=new Date("24 september 2023 23:59:00");
+
+    const currenttime=new Date();
+
+    const totalTime=eventTime-currenttime;
+
+    
+
+
+    let totalSecond=Math.floor(totalTime/1000);
+    let totalMin=Math.floor(totalSecond/60);
+    let totalHour=Math.floor(totalMin/60);
+
+
+    console.log("total ::"+totalHour+" "+totalMin+" "+totalSecond);
+
+    let days=Math.floor(totalHour/24);
+    let hours=totalHour%24;
+    let minyues=totalMin%60;
+    let seconds=totalSecond%60;
+
+    console.log("pppppp ::"+totalHour+" "+totalMin+" "+totalSecond)
+
+
+    day.innerHTML=days;
+    Hour.innerHTML=hours;
+    Min.innerHTML=minyues;
+    Second.innerHTML=seconds;
+
+}
+
+
+CountDown();
+
+setInterval(CountDown,1000);
